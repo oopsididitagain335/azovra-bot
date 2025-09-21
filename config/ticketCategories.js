c2683c3dea577f189ccba4ef4d9aaf86b198c0be
Solace <monkdev9@gmail.com>
2025-09-21 22:54:43 +0100

    Create ticketCategories.js

---
+++ config/ticketCategories.js
@@ -0,0 +1,35 @@
+// config/ticketCategories.js
+module.exports = {
+  categories: [
+    {
+      label: 'General Support',
+      value: 'general-support',
+      description: 'Any reports, general inquiries or questions.',
+      categoryName: '🎫・General Support'
+    },
+    {
+      label: 'Role Request',
+      value: 'role-request',
+      description: 'Request a Role',
+      categoryName: '🎫・Role Requests'
+    },
+    {
+      label: 'Org Role Creation',
+      value: 'org-role-creation',
+      description: 'Create a role for your organization/business.',
+      categoryName: '🎫・Org Roles'
+    },
+    {
+      label: 'Contact Owners',
+      value: 'contact-owners',
+      description: 'Important Matters, Investments or Collaborations / Sponsors.',
+      categoryName: '🎫・Owner Contact'
+    },
+    {
+      label: 'Staff Application',
+      value: 'staff-application',
+      description: 'Applying for staff or management.',
+      categoryName: '🎫・Staff Applications'
+    }
+  ]
+};
