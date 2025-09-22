4bdfc3650f58f44edfd1769fee6ac7e646993f75
Solace <monkdev9@gmail.com>
2025-09-22 16:08:39 +0100

    Update sec.js

--- commands/tickets/sec.js
+++ commands/tickets/sec.js
@@ -9,7 +9,7 @@ const {
 const ticketCategories = require('../../config/ticketCategories.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
+   new SlashCommandBuilder()
     .setName('sec')
     .setDescription('🔐 Send or update the persistent ticket panel.'),
 
